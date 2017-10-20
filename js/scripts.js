2b6c7a8daa40e8200231269fc236829dc992eb87
Guest User <Guest@Epicodus-10E.local>
2017-10-20 13:52:45 -0700

    Refine styles, add greeting with user name

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-$(document).ready(function() {
+var greeting = function (_message, _nameInput) {
+  return _message + _nameInput;
+};
 
+$(document).ready(function() {
   $("form#survey").submit(function(event) {
+    debugger
     event.preventDefault()
     var choice = parseInt($("#goal").val());
+    var message = "Thanks for completing this questionnaire, ";
+    var nameInput = $("input#name").val();
+    var greeting = message.concat(nameInput);
+
+    $(".greeting").text(greeting);
 
     if (choice === 1) {
       $("#c").show();
@@ -12,9 +21,12 @@ $(document).ready(function() {
 
     } else if (choice === 3) {
       $("#php").show();
+
     }
     else {
       $("#ruby").show();
+
     }
   });
+
 });
